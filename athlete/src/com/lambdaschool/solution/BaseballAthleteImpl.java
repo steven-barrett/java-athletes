de190f2af5f5b67b9b87415552192d030b648ff3
steven122185 <49796406+steven122185@users.noreply.github.com>
2019-07-11 18:18:24 -0400

    Create the 4 corresponding implementation classes

---
+++ athlete/src/com/lambdaschool/solution/BaseballAthleteImpl.java
@@ -0,0 +1,11 @@
+package com.lambdaschool.solution;
+
+public class BaseballAthleteImpl implements AthleteCreation
+{
+    @Override
+    public void displayAthlete()
+    {
+        System.out.println("Baseball Athlete!");
+    }
+
+}
